1eef3a81c31535e99870289654978ed658c546fc
kisakyegordon <gordon56692@gmail.com>
2018-12-19 11:44:14 +0300

    first commit

---
+++ src/__tests__/test.spec.js
@@ -0,0 +1,12 @@
+// import { shallow, configure } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+// const PhoneGenerator = require('./phoneGenerator');
+
+// configure({ adapter: new Adapter() });
+
+describe("Phone Generator", () => {
+    it("tests for phone generator", () => {
+        // const wrapper = shallow(<Nav />);
+        expect(true).toBe(true);
+    })
+})
